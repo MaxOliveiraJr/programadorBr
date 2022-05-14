0b8fc3b26f4841bf5b9824afa595686aa67dfc1a
Max Oliveira <max-oliveira@outlook.com.br>
2022-05-13 21:54:31 -0300

    Atualizado de Jogo da Memoria

--- Modulo 17/firabase/scripts/app.js
+++ Modulo 17/firabase/scripts/app.js
@@ -24,10 +24,152 @@ let db = firebase.firestore();
     */
 
 
-let docRef = db.collection('turmaA').doc("nojN2sGyvEsKT3ltW2dW")
+// let docRef = db.collection('turmaA').doc("nojN2sGyvEsKT3ltW2dW")
 
-docRef.get()
-    .then((doc) => {
-        let aluno = doc.data();
-        console.log(aluno.nome)
+// docRef.get()
+//     .then((doc) => {
+//         let aluno = doc.data();
+//         console.log(aluno.nome)
+//     })
+
+
+
+db.collection("turmaA").where("nome", ">", "B")
+    .where("nome", "<", "Zuleide")
+    .get()
+    .then(snapshot => {
+
+        snapshot.forEach((doc) => {
+
+            let aluno = doc.data();
+            console.log(aluno);
+        })
+
     })
+
+
+
+
+db.collection("turmaA").where("notas.nota1", ">", 5)
+    .get()
+    .then(snapshot => {
+
+        snapshot.forEach((doc) => {
+
+            let aluno = doc.data();
+            console.log(aluno);
+        })
+
+    })
+
+
+const TURMA = "turmaA";
+/*
+db.collection(TURMA).add({
+    nome: "Marcos",
+    sobrenome: "Santos",
+    notas: {
+        nota1: 9.6,
+        nota2: 7.5
+    },
+    advertencias: [
+
+        {desc: "fugiu", data: "21.02.2019"},
+        {desc: "Não veio", data: "21.02.2019"}
+
+    ]
+
+
+}).then(doc => {
+    console.log('Documento iserido com sucesso');
+}).catch(error => {
+    console.log(error)
+})
+
+
+*/
+
+/*
+db.collection(TURMA).doc("Alunonovo").set(
+    {
+        nome: "Mariana",
+        sobrenome: "Olivera",
+        notas: { nota1: 9.6, nota2: 7.5 }
+    }
+).then(doc => {
+    console.log('Documennto')
+})*/
+
+
+
+db.collection(TURMA).doc("Alunonovo").set(
+    {
+
+        sobrenome: "Oliveira",
+
+    }, { merge: true }
+).then(doc => {
+    console.log('Documennto')
+})
+
+
+
+
+db.collection(TURMA).doc("sAw2AzJj3jfdIZVGMayt").update(
+    {
+
+        sobrenome: "Oliveira Santos",
+
+    }
+).then(doc => {
+    console.log('Documento')
+})
+
+
+
+db.collection(TURMA).doc("Alunonovo").update(
+    {
+
+        cidades:['Minas Gerais']
+
+    }
+).then(doc => {
+    console.log('Documennto')
+})
+
+
+
+
+db.collection(TURMA).doc("Alunonovo").update(
+    {
+
+        cidades: firebase.firestore.FieldValue.arrayUnion("Vitoria","Pernanbuco")
+
+    }
+).then(doc => {
+    console.log('Documennto')
+})
+
+
+
+db.collection(TURMA).doc("Alunonovo").update(
+    {
+
+        cidades: firebase.firestore.FieldValue.arrayRemove("Vitoria")
+
+    }
+).then(doc => {
+    console.log('Documennto')
+})
+
+
+
+db.collection(TURMA).doc("Alunonovo").update(
+    {
+
+        faltas: firebase.firestore.FieldValue.increment(1)
+
+    }
+).then(doc => {
+    console.log('Documennto')
+})
